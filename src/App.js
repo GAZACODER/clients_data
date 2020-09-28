a116841cb0c29e4960afba029ef69664db02c0be
ahmadysalah <a.ahmad.salah@gmail.com>
2020-09-28 16:59:02 +0300

    Logic local storage function #10

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import InputText from './components/TextInput';
+import StorageData from './utils/LocalStorage';
+import Swal from 'sweetalert2';
 
 import './App.css';
 
@@ -7,32 +9,45 @@ const App = () => {
   const [firstName, setFirstName] = useState();
   const [LastName, setLastName] = useState();
   const [age, setAge] = useState();
+  const [data, setData] = useState(StorageData.GetLocal() || []);
 
-  const handelClick = () => {};
+  const handelClick = () => {
+    if (!firstName || !LastName || !age)
+      Swal.fire({
+        title: 'Error!',
+        text: 'Check data please',
+        icon: 'error',
+        confirmButtonText: 'OK',
+      });
+    else {
+      setData([...data, { firstName, LastName, age }]);
+      StorageData.AddLocal([...data, { firstName, LastName, age }]);
+    }
+  };
 
   return (
-    <div className="container">
-      <form className="Text__input__form">
+    <div className='container'>
+      <div className='container__form'>
         <InputText
-          placeholder="enter first name "
+          placeholder='enter first name '
           value={firstName}
           change={setFirstName}
         />
         <InputText
-          placeholder="enter last name "
+          placeholder='enter last name '
           value={LastName}
           change={setLastName}
         />
         <InputText
-          type="number"
-          placeholder="enter age "
+          type='number'
+          placeholder='enter age '
           value={age}
           change={setAge}
         />
-        <button className="form__layout__button" onClick={() => handelClick()}>
+        <button className='form__layout__button' onClick={() => handelClick()}>
           submit
         </button>
-      </form>
+      </div>
     </div>
   );
 };
