fde131c8cc28a44205d0f221730d197c0a7b9344
mohsalah1997 <53860979+mohsalah1997@users.noreply.github.com>
2020-09-28 20:34:22 +0300

    add table component #6

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import './style.css';
+
+const Table = ({ data }) => {
+  const handelDelete = () => {};
+  const handelEdit = () => {};
+  return (
+    <div>
+      <table className="table__todo">
+        <tr>
+          <th>num </th>
+          <th>first name</th>
+          <th>last name</th>
+          <th>age</th>
+          <th>delete</th>
+          <th>edit</th>
+        </tr>
+
+        {data &&
+          data.map(({ firstName, lastName, age }, index) => (
+            <tr>
+              <td>{index + 1}</td>
+              <td>{firstName}</td>
+              <td> {lastName}</td>
+              <td>{age}</td>
+              <td>
+                <button
+                  onClick={() => handelDelete()}
+                  className="table__todo__delete__btn"
+                >
+                  delete
+                </button>
+              </td>
+              <td>
+                <button
+                  onClick={() => handelEdit()}
+                  className="table__todo__edit__btn"
+                >
+                  edit
+                </button>
+              </td>
+            </tr>
+          ))}
+      </table>
+    </div>
+  );
+};
+export default Table;
