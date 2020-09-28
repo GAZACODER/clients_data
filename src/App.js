bc836b2f71dec5bd4d715bdbe7a906208b1d2e3d
mohsalah1997 <53860979+mohsalah1997@users.noreply.github.com>
2020-09-28 14:53:44 +0300

    add form input #5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputText from './components/TextInput';
 
 import './App.css';
 
 const App = () => {
-  const x = 15;
-  return <InputText />;
+  const [firstName, setFirstName] = useState();
+  const [LastName, setLastName] = useState();
+  const [age, setAge] = useState();
+
+  return (
+    <form className="Text__input__form">
+      <InputText
+        placeholder="enter first name "
+        value={firstName}
+        change={setFirstName}
+      />
+      <InputText
+        placeholder="enter last name "
+        value={LastName}
+        change={setLastName}
+      />
+      <InputText placeholder="enter age " value={age} change={setAge} />
+      <button className="form__layout__button">submit</button>
+    </form>
+  );
 };
 
 export default App;
