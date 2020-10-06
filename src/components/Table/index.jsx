f208d749b5c1f3e20e7a5c16d35b2e410d4a6d37
mohsalah1997 <53860979+mohsalah1997@users.noreply.github.com>
2020-10-06 13:30:34 +0300

    Doing the pages feature and moving between these pages by adding a button to the previous page and a button for the next page, and adding the necessary conditions to move between pages #16

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import './style.css';
 import StorageData from '../../utils/LocalStorage';
 import SweetAlert from '../../utils/SweetAlert';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 let newData = [];
 const Table = ({ data, changeData }) => {
+  const [page, setPage] = useState(0);
   const handelDelete = (i) => {
     SweetAlert.ConfirmWithAlert(() => {
       newData = data.filter((x, index) => index !== i);
@@ -27,31 +30,52 @@ const Table = ({ data, changeData }) => {
         </tr>
 
         {data &&
-          data.map(({ firstName, LastName, age }, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{firstName}</td>
-              <td> {LastName}</td>
-              <td>{age}</td>
-              <td>
-                <button
-                  onClick={() => handelDelete(index)}
-                  className="table__todo__delete__btn"
-                >
-                  delete
-                </button>
-              </td>
-              <td>
-                <button
-                  onClick={() => handelEdit()}
-                  className="table__todo__edit__btn"
-                >
-                  edit
-                </button>
-              </td>
-            </tr>
-          ))}
+          data.map(
+            ({ firstName, LastName, age }, index) =>
+              index >= page &&
+              index <= page + 6 && (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{firstName}</td>
+                  <td> {LastName}</td>
+                  <td>{age}</td>
+                  <td>
+                    <button
+                      onClick={() => handelDelete(index)}
+                      className="table__todo__delete__btn"
+                    >
+                      delete
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handelEdit()}
+                      className="table__todo__edit__btn"
+                    >
+                      edit
+                    </button>
+                  </td>
+                </tr>
+              )
+          )}
       </table>
+
+      <div className="footer__icon">
+        <FontAwesomeIcon
+          className="arrow__icon"
+          icon={faArrowLeft}
+          color="#4CAF50"
+          size="2x"
+          onClick={() => page > 0 && setPage(page - 7)}
+        />
+        <FontAwesomeIcon
+          className="arrow__icon"
+          icon={faArrowRight}
+          color="#4CAF50"
+          size="2x"
+          onClick={() => page + 7 < data.length && setPage(page + 7)}
+        />
+      </div>
     </div>
   );
 };
