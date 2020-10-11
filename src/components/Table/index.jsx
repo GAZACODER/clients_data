ecf05898363d41c7ea5ca5b0215cb17838a28153
mohsalah1997 <53860979+mohsalah1997@users.noreply.github.com>
2020-10-11 12:50:02 +0300

    Perform the sorting feature on the client data, where the order is ascending and descending #17

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -3,17 +3,60 @@ import './style.css';
 import StorageData from '../../utils/LocalStorage';
 import SweetAlert from '../../utils/SweetAlert';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowLeft,
+  faArrowRight,
+  faSortDown,
+  faSortUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 let newData = [];
 const Table = ({ data, changeData }) => {
   const [page, setPage] = useState(0);
-  const handelDelete = (i) => {
+
+  const icon = (sortedColumn, type) =>
+    type === 'up' ? (
+      <FontAwesomeIcon
+        className="Sorting__icon"
+        icon={faSortUp}
+        color="white"
+        onClick={() => setSorted(sortedColumn, type)}
+      />
+    ) : (
+      <FontAwesomeIcon
+        className="Sorting__icon"
+        icon={faSortDown}
+        color="white"
+        onClick={() => setSorted(sortedColumn, type)}
+      />
+    );
+
+  const handelDelete = (deleteId) => {
+    console.log(deleteId);
     SweetAlert.ConfirmWithAlert(() => {
-      newData = data.filter((x, index) => index !== i);
+      newData = data.filter(({ id }) => id !== deleteId);
       changeData(newData);
       StorageData.AddLocal(newData);
     }, 'warning');
+  };
+
+  const setSorted = (sortedColumn, type) => {
+    const comparer = (a, b) => {
+      if (sortedColumn === 'firstName' || sortedColumn === 'LastName') {
+        if (type === 'up') {
+          return a[sortedColumn].localeCompare(b[sortedColumn]);
+        } else return b[sortedColumn].localeCompare(a[sortedColumn]);
+      } else {
+        console.log(sortedColumn);
+        if (type === 'down') {
+          return Number(a[sortedColumn]) > Number(b[sortedColumn]) ? 1 : -1;
+        } else {
+          return Number(a[sortedColumn]) < Number(b[sortedColumn]) ? 1 : -1;
+        }
+      }
+    };
+
+    changeData([...data].sort(comparer));
   };
 
   const handelEdit = () => {};
@@ -22,16 +65,40 @@ const Table = ({ data, changeData }) => {
       <table className="table__todo">
         <tr>
           <th>num </th>
-          <th>first name</th>
-          <th>last name</th>
-          <th>age</th>
+          <th>
+            <div className="Th__Sorting">
+              first name
+              <div className="Sorting">
+                {icon('firstName', 'up')}
+                {icon('firstName', 'down')}
+              </div>
+            </div>
+          </th>
+          <th>
+            <div className="Th__Sorting">
+              last name
+              <div className="Sorting">
+                {icon('LastName', 'up')}
+                {icon('LastName', 'down')}
+              </div>
+            </div>
+          </th>
+          <th>
+            <div className="Th__Sorting">
+              age
+              <div className="Sorting">
+                {icon('age', 'up')}
+                {icon('age', 'down')}
+              </div>
+            </div>
+          </th>
           <th>delete</th>
           <th>edit</th>
         </tr>
 
         {data &&
           data.map(
-            ({ firstName, LastName, age }, index) =>
+            ({ id, firstName, LastName, age }, index) =>
               index >= page &&
               index <= page + 6 && (
                 <tr key={index}>
@@ -41,7 +108,7 @@ const Table = ({ data, changeData }) => {
                   <td>{age}</td>
                   <td>
                     <button
-                      onClick={() => handelDelete(index)}
+                      onClick={() => handelDelete(id)}
                       className="table__todo__delete__btn"
                     >
                       delete
